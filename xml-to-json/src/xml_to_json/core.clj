422cf0f2fa88bf4c9459e34998b35d8e3439ca04
naomijub <jnboeira@outlook.com>
2017-02-13 19:11:47 -0300

    lein app

---
+++ xml-to-json/src/xml_to_json/core.clj
@@ -0,0 +1,7 @@
+(ns xml-to-json.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ xml-to-json/test/xml_to_json/core_test.clj
@@ -0,0 +1,7 @@
+(ns xml-to-json.core-test
+  (:require [clojure.test :refer :all]
+            [xml-to-json.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
